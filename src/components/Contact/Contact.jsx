141d6e11c1b905c65623e0c2c775394ba4961f68
adeebtamer132 <adeebtamer132@gmail.com>
2024-02-19 12:18:01 +0200

    link contact mail

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -33,25 +33,25 @@ export default function Contact() {
           />
         </svg>
       ),
-      contact: "Marketing@serbaljazira.com",
+      contact: <a className="border border-solid border-gray-300 rounded-lg font-extrabold bg-gradient-to-l from-[#3cafbb] to-[#2a426b] pr-[10px] pl-[10px] " href="mailto:Marketing@serbaljazira.com">Marketing@serbaljazira.com</a>,
     },
     {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          {/* <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-          /> */}
-        </svg>
-      ),
+      // icon: (
+      //   <svg
+      //     xmlns="http://www.w3.org/2000/svg"
+      //     fill="none"
+      //     viewBox="0 0 24 24"
+      //     strokeWidth={1.5}
+      //     stroke="currentColor"
+      //     className="w-6 h-6"
+      //   >
+      //     <path
+      //       strokeLinecap="round"
+      //       strokeLinejoin="round"
+      //       d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+      //     />
+      //   </svg>
+      // ),
       // contact: "+1 (555) 000-000",
     },
     {
@@ -112,7 +112,7 @@ export default function Contact() {
                 <iframe
                   style={iframeStyle}
                   frameBorder="0"
-                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.3469906195295!2d46.7287761!3d24.6624446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f0424ff2e530f%3A0x36529ba249766c8f!2sAl%20Malaz%2C%20Riyadh%2C%20Saudi%20Arabia!5e0!3m2!1sen!2sus!4v1645159959914!5m2!1sen!2sus"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.3469906195295!2d46.7287761!3d24.6624446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f0424ff2e530f%3A0x36529ba249766c8f!2sAl%20Malaz%2C%20Riyadh%2C%20Saudi%20Arabia!5e0!3m2!1sen!2sus!4v1645159959914!5m2!1sen!2sus"
                   title="Map of Saudi Arabia "
                 />
               </div>
